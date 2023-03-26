6f903a93e0a6953eb6641d56162da090a478ed57
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-03-26 20:21:46 +0700

    menambahkan class SayaTubeVideo

---
+++ TP_MOD6_1302213038/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD6_1302213038
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random random = new Random();
+            this.id = random.Next(10000,99999);
+            this.playCount = 0;
+        }
+
+        public void increasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID Video\t: " + this.id);
+            Console.WriteLine("Judul Video\t: " + this.title);
+            Console.WriteLine("View Count\t: " + this.playCount);
+        }
+    }
+}
--- TP_MOD6_1302213038/Program.cs
+++ TP_MOD6_1302213038/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using TP_MOD6_1302213038;
+
+class main
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial " +
+            "Design by Contract - Aisha Putri Nuryan");
+        vid.increasePlayCount(1864);
+        vid.printVideoDetails();
+    }
+}
